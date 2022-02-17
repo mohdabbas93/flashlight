f68c6279e9b5b29ea70068f0116ae55deddfdff9
Mohammad Abbas <mohdabbas@narbase.com>
2022-02-17 21:40:19 +0400

    Setup the light switch button to open and close the rear camera torch

--- app/src/main/java/com/mohdabbas/flashlight/MainActivity.kt
+++ app/src/main/java/com/mohdabbas/flashlight/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.mohdabbas.flashlight
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mohdabbas.flashlight.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var isLightOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+
+        try {
+            val cameraId = cameraManager.cameraIdList[0]
+            binding.lightSwitchButton.setOnClickListener {
+                try {
+                    cameraManager.setTorchMode(cameraId, !isLightOn)
+                    binding.lightSwitchButton.text = if (isLightOn) "Open" else "Close"
+                    isLightOn = isLightOn.not()
+                } catch (e: CameraAccessException) {
+                    e.printStackTrace()
+                }
+            }
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
     }
 }
